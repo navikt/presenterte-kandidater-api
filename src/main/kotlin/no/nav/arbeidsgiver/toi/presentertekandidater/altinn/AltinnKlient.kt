fe9e1bc61b2145c34060b0640b17697bbeb58fec
noraness <nora.storebo.ness@nav.no>
2022-11-23 12:42:47 +0100

    Lag altinnklient

---
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/altinn/AltinnKlient.kt
@@ -0,0 +1,28 @@
+package no.nav.arbeidsgiver.toi.presentertekandidater.altinn
+
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlient
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlientConfig
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.ProxyConfig
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.SelvbetjeningToken
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.Subject
+import no.nav.arbeidsgiver.toi.presentertekandidater.variable
+
+
+class AltinnKlient(envs: Map<String, String>) {
+
+    private val consumerId = envs.variable("NAIS_APPLICATION_NAME")
+    private val altinnProxyUrl = envs.variable("ALTINN_PROXY_URL")
+    private val scope = envs.variable("ALTINN_PROXY_AUDIENCE")
+
+    val config = AltinnrettigheterProxyKlientConfig(ProxyConfig(consumerId, altinnProxyUrl))
+    val klient = AltinnrettigheterProxyKlient(config)
+
+    fun hentOrganisasjoner(fnr: String, hentExchangeToken: (fnr: String, scope: String) -> String) =
+        klient.hentOrganisasjoner(
+            SelvbetjeningToken(hentExchangeToken(fnr, scope)),
+            Subject(fnr),
+            true
+        )
+
+
+}
