458be64d78bd3b609458a0ed429c2da4b656a493
Mads Lee Giil <mads.lee.giil@nav.no>
2022-11-24 12:16:46 +0100

    Fiks token utstedt i test

--- src/test/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/tokenUtils.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/tokenUtils.kt
@@ -3,7 +3,7 @@ package no.nav.arbeidsgiver.toi.presentertekandidater
 import no.nav.security.mock.oauth2.MockOAuth2Server
 
 fun hentToken(mockOAuth2Server: MockOAuth2Server): String {
-    return mockOAuth2Server.issueToken().serialize()
+    return mockOAuth2Server.issueToken(claims = mapOf("pid" to "01838699827")).serialize()
 }
 
 fun hentUgyldigToken(mockOAuth2Server: MockOAuth2Server): String {
