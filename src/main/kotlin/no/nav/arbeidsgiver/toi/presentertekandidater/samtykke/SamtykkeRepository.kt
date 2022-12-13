ec9d416910bc4015ec8d64b89481057c8da48c1c
noraness <nora.storebo.ness@nav.no>
2022-12-13 12:32:27 +0100

    Lagre og hent samtykke fra database

---
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/samtykke/SamtykkeRepository.kt
@@ -0,0 +1,30 @@
+package no.nav.arbeidsgiver.toi.presentertekandidater.samtykke
+
+import javax.sql.DataSource
+
+class SamtykkeRepository(private val dataSource: DataSource) {
+
+    fun lagre(fødselsnummer: String) {
+        dataSource.connection.use {
+            val sql = """
+                insert into samtykke(fødselsnummer, opprettet) 
+                values (?, current_timestamp())
+            """.trimIndent()
+
+            it.prepareStatement(sql).apply {
+                this.setString(1, fødselsnummer)
+            }.execute()
+        }
+    }
+
+    fun harSamtykket(fødselsnummer: String): Boolean =
+        dataSource.connection.use {
+            val sql = """
+                select 1 from samtykke where fødselsnummer = ?
+            """.trimIndent()
+
+            it.prepareStatement(sql).apply {
+                this.setString(1, fødselsnummer)
+            }.executeQuery().next()
+        }
+}
