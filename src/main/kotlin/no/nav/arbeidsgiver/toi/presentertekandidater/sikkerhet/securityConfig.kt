020916ad729333a2e10af43bcfbf5230df3c7c29
Mads Lee Giil <mads.lee.giil@nav.no>
2022-12-20 12:17:21 +0100

    WIP: endre krav til rolle

--- src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/sikkerhet/securityConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/sikkerhet/securityConfig.kt
@@ -4,13 +4,13 @@ import io.javalin.core.security.RouteRole
 import io.javalin.http.Context
 import io.javalin.http.HttpResponseException
 import no.nav.arbeidsgiver.toi.presentertekandidater.altinn.AltinnKlient
+import no.nav.arbeidsgiver.toi.presentertekandidater.hentFødselsnummer
 import no.nav.arbeidsgiver.toi.presentertekandidater.navalin.AccessToken
 import no.nav.arbeidsgiver.toi.presentertekandidater.navalin.NavalinAccessManager.TokenUtsteder.INGEN
 import no.nav.arbeidsgiver.toi.presentertekandidater.navalin.NavalinAccessManager.TokenUtsteder.TOKEN_X
 import no.nav.arbeidsgiver.toi.presentertekandidater.navalin.RolleKonfigurasjon
 import no.nav.arbeidsgiver.toi.presentertekandidater.samtykke.SamtykkeRepository
 import no.nav.arbeidsgiver.toi.presentertekandidater.setFødselsnummer
-import no.nav.arbeidsgiver.toi.presentertekandidater.setOrganisasjoner
 import no.nav.arbeidsgiver.toi.presentertekandidater.setOrganisasjonerForRekruttering
 import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import org.eclipse.jetty.http.HttpStatus
@@ -20,12 +20,18 @@ fun konfigurerRoller(altinnKlient: AltinnKlient, samtykkeRepository: SamtykkeRep
     RolleKonfigurasjon(
         rolle = Rolle.ARBEIDSGIVER,
         tokenUtsteder = TOKEN_X,
-        autoriseringskrav = autentiserArbeidsgiver(altinnKlient, samtykkeRepository, false)
+        autoriseringskrav = hentRepresenterteOrganisasjoner(altinnKlient, samtykkeRepository, true)
+
     ),
     RolleKonfigurasjon(
         rolle = Rolle.ARBEIDSGIVER_MED_ROLLE_REKRUTTERING,
         tokenUtsteder = TOKEN_X,
-        autoriseringskrav = autentiserArbeidsgiver(altinnKlient, samtykkeRepository, true)
+        autoriseringskrav = sjekkSamtykkeOgRolleRekruttering(altinnKlient, samtykkeRepository)
+    ),
+    RolleKonfigurasjon(
+        rolle = Rolle.EKSTERN_ARBEIDSGIVER,
+        tokenUtsteder = TOKEN_X,
+        autoriseringskrav = sjekkRepresentererOrganisasjonMedRolleRekruttering(altinnKlient, samtykkeRepository)
     ),
     RolleKonfigurasjon(
         rolle = Rolle.UNPROTECTED,
@@ -34,39 +40,79 @@ fun konfigurerRoller(altinnKlient: AltinnKlient, samtykkeRepository: SamtykkeRep
 )
 
 enum class Rolle : RouteRole {
-    ARBEIDSGIVER, UNPROTECTED, ARBEIDSGIVER_MED_ROLLE_REKRUTTERING
+    ARBEIDSGIVER, UNPROTECTED, ARBEIDSGIVER_MED_ROLLE_REKRUTTERING, EKSTERN_ARBEIDSGIVER
 }
 
-val åpent: (JwtTokenClaims, Context, AccessToken) -> Boolean = {
-    jwtTokenClaims, context, accessToken -> true
-}
-
-val autentiserArbeidsgiver: (AltinnKlient, SamtykkeRepository, Boolean) -> (JwtTokenClaims, Context, AccessToken) -> Boolean =
-    { altinnKlient, samtykkeRepository, forRolleRekruttering ->
+val sjekkSamtykkeOgRolleRekruttering : (AltinnKlient, SamtykkeRepository) -> (JwtTokenClaims, Context, AccessToken) -> Boolean =
+    { altinnKlient, samtykkeRepository ->
         { jwtTokenClaims, context, accessToken ->
-            val fødselsnummerClaim = jwtTokenClaims.get("pid")
 
-            if (fødselsnummerClaim == null) {
+            val harRolleRekruttering = sjekkRepresentererOrganisasjonMedRolleRekruttering(altinnKlient, samtykkeRepository)(jwtTokenClaims, context, accessToken)
+            val harSamtykket = sjekkSamtykke(samtykkeRepository)(jwtTokenClaims, context, accessToken)
+            harRolleRekruttering && harSamtykket
+        }
+    }
+
+val sjekkSamtykke: (SamtykkeRepository) -> (JwtTokenClaims, Context, AccessToken) -> Boolean =
+    { samtykkeRepository ->
+        { jwtTokenClaims, context, accessToken ->
+            settFødselsnummerPåKontekst(jwtTokenClaims, context)
+            val fnr = context.hentFødselsnummer()
+
+            val harSamtykketVilkår = samtykkeRepository.harSamtykket(fnr)
+            if (!harSamtykketVilkår) {
+                throw UnavailableForLegalReasons()
+            }
+            true
+        }
+    }
+
+val sjekkRepresentererOrganisasjonMedRolleRekruttering: (AltinnKlient, SamtykkeRepository) -> (JwtTokenClaims, Context, AccessToken) -> Boolean =
+    { altinnKlient, samtykkeRepository ->
+        { jwtTokenClaims, context, accessToken ->
+            settFødselsnummerPåKontekst(jwtTokenClaims, context)
+            val fnr = context.hentFødselsnummer()
+
+            val organisasjoner =
+                altinnKlient.hentOrganisasjonerMedRettighetRekrutteringFraAltinn(fnr, accessToken)
+
+            if (organisasjoner.isEmpty()) {
                 false
             } else {
-                val fnr = fødselsnummerClaim.toString()
-                context.setFødselsnummer(fnr)
-
-                if (forRolleRekruttering) {
-                    val harSamtykketVilkår = samtykkeRepository.harSamtykket(fnr)
-                    if (!harSamtykketVilkår) {
-                        throw UnavailableForLegalReasons()
-                    }
-                    val organisasjoner = altinnKlient.hentOrganisasjonerMedRettighetRekrutteringFraAltinn(fnr, accessToken)
-                    context.setOrganisasjonerForRekruttering(organisasjoner)
-                } else {
-                    val organisasjoner = altinnKlient.hentOrganisasjoner(fnr, accessToken)
-                    context.setOrganisasjoner(organisasjoner)
-                }
+                context.setOrganisasjonerForRekruttering(organisasjoner)
                 true
             }
         }
     }
+
+val hentRepresenterteOrganisasjoner: (AltinnKlient, SamtykkeRepository, Boolean) -> (JwtTokenClaims, Context, AccessToken) -> Boolean =
+    { altinnKlient, samtykkeRepository, forRolleRekruttering ->
+        { jwtTokenClaims, context, accessToken ->
+            settFødselsnummerPåKontekst(jwtTokenClaims, context)
+            val fnr = context.hentFødselsnummer()
+
+            val organisasjoner =
+                altinnKlient.hentOrganisasjoner(fnr, accessToken)
+
+            if (organisasjoner.isNotEmpty()) {
+                context.setOrganisasjonerForRekruttering(organisasjoner)
+
+            }
+                true
+
+        }
+    }
+
+fun settFødselsnummerPåKontekst(claims: JwtTokenClaims, context: Context) {
+    val fødselsnummerClaim = claims.get("pid")
+
+    if (fødselsnummerClaim == null) {
+        false // TODO
+    } else {
+        val fnr = fødselsnummerClaim.toString()
+        context.setFødselsnummer(fnr)
+    }
+}
 
 class UnavailableForLegalReasons @JvmOverloads constructor(
     message: String = "Unavailable for legal reasons",
