0cc7cef929e7cc6d8bbd40fe5f29e35559a4216d
Mads Lee Giil <mads.lee.giil@nav.no>
2023-03-08 09:58:35 +0100

    Skriv tester for publisering av meldinger for vist kontaktinfo

---
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/visningkontaktinfo/VisningKontaktinfoPubliserer.kt
@@ -0,0 +1,15 @@
+package no.nav.arbeidsgiver.toi.presentertekandidater.visningkontaktinfo
+
+import no.nav.helse.rapids_rivers.RapidsConnection
+
+class VisningKontaktinfoPubliserer(
+    rapidsConnection: RapidsConnection,
+    visningKontaktinfoRepository: VisningKontaktinfoRepository
+) {
+
+    fun publiser() {
+
+    }
+
+
+}
--- src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/visningkontaktinfo/VisningKontaktinfoRepository.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/visningkontaktinfo/VisningKontaktinfoRepository.kt
@@ -1,5 +1,7 @@
 package no.nav.arbeidsgiver.toi.presentertekandidater.visningkontaktinfo
 
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.util.*
 import javax.sql.DataSource
 
@@ -14,4 +16,41 @@ class VisningKontaktinfoRepository(private val dataSource: DataSource) {
             setObject(2, stillingId)
         }.executeUpdate() > 0
     }
+
+    fun hentAlleRegistrerteVisninger(): List<RegistrertVisning>  = dataSource.connection.use {
+        val resultSet = it.prepareStatement("select * from visning_kontaktinfo")
+            .executeQuery()
+
+        return generateSequence {
+            if (resultSet.next()) {
+                RegistrertVisning(
+                    id = resultSet.getLong("id"),
+                    aktørId = resultSet.getString("aktør_id"),
+                    stillingsId = resultSet.getObject("stilling_id") as UUID,
+                    tidspunkt = resultSet.getTimestamp("tidspunkt").toInstant().atZone(ZoneId.of("Europe/Oslo")),
+                    publisertMelding = resultSet.getBoolean("publisert_melding")
+                )
+            } else null
+        }.toList()
+    }
+
+    fun markerSomPublisert(registrertVisning: RegistrertVisning) {
+        dataSource.connection.use {
+            it.prepareStatement("""
+                update visning_kontaktinfo
+                set publisert_melding = true
+                where id = ?
+            """.trimIndent()).apply {
+                setLong(1, registrertVisning.id)
+            }.use { statement -> statement.executeUpdate() }
+        }
+    }
+
+    data class RegistrertVisning(
+        val id: Long,
+        val aktørId: String,
+        val stillingsId: UUID,
+        val tidspunkt: ZonedDateTime,
+        val publisertMelding: Boolean
+    )
 }
