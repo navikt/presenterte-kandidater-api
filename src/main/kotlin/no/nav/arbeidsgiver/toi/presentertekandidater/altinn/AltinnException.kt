0e9d2ad013f9b077b36a34fbd8051a42ad643540
Oskar Hvalstad-Nilsen <oskar.hvalstad-nilsen@nav.no>
2025-11-19 14:25:20 +0100

    Fiks typo

--- src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/altinn/AltinnException.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/toi/presentertekandidater/altinn/AltinnException.kt
@@ -1,4 +1,4 @@
-package no.nav.arbeidsgiver.toi.presentertekandidater.altinn;
+package no.nav.arbeidsgiver.toi.presentertekandidater.altinn
 
 sealed class AltinnException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
 
